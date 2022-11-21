b7483d6e5eb8451b60c9fdd63b4225b2badb69d1
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-21 14:04:48 +0530

    Use Case 1 implemented

---
+++ AdressBook/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook
+{
+    internal class Contacts
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string phone;
+        public string city;
+        public string state;
+        public string zip;
+        public string country;
+    }
+}
--- AdressBook/Program.cs
+++ AdressBook/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdressBook;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,40 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program in AddressBook Main class on START Master Branch");
+            List<Contacts> adressBook = new List<Contacts>();
+            Contacts contact = new Contacts();
+            Console.WriteLine("enter the First Name:");
+            contact.firstName=Console.ReadLine();
+            adressBook.Add(contact);
+
+            Console.WriteLine("enter the Last Name:");
+            contact.lastName=Console.ReadLine();
+            adressBook.Add(contact);
+
+            Console.WriteLine("enter the email:");
+            contact.email=Console.ReadLine();
+            adressBook.Add(contact);
+
+            Console.WriteLine("enter the phone no:");
+            contact.phone=Console.ReadLine();
+            adressBook.Add(contact);
+
+            Console.WriteLine("enter the city name:");
+            contact.city=Console.ReadLine();
+            adressBook.Add(contact);
+
+            Console.WriteLine("enter the State:");
+            contact.state = Console.ReadLine();
+            adressBook.Add(contact);
+
+            Console.WriteLine("enter the zip code:");
+            contact.zip=Console.ReadLine();
+            adressBook.Add(contact);
+
+            Console.WriteLine("enter the country:");
+            contact.country=Console.ReadLine();
+            adressBook.Add(contact);
+
             Console.ReadLine();
         }
     }
