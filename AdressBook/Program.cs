b57aae050bd05487fb110f9e56ffdbe554c73bff
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-21 13:23:03 +0530

    Added welcome message

---
+++ AdressBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program in AddressBook Main class on START Master Branch");
+            Console.ReadLine();
+        }
+    }
+}
