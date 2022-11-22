1934856e1e676297944d8bc38d66373b1c934540
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-22 12:54:43 +0530

    Use case 2 modified

--- AdressBook/Program.cs
+++ AdressBook/Program.cs
@@ -12,45 +12,12 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program in AddressBook Main class on START Master Branch");
-            List<Contacts> adressBook = new List<Contacts>();
-            Contacts contact = new Contacts();
+            ManageContacts.addContact();
 
-            Console.WriteLine("enter the First Name:");
-            contact.firstName=Console.ReadLine();
-
-            Console.WriteLine("enter the Last Name:");
-            contact.lastName=Console.ReadLine();
-            
-
-            Console.WriteLine("enter the email:");
-            contact.email=Console.ReadLine();
-            
-
-            Console.WriteLine("enter the phone no:");
-            contact.phone=Console.ReadLine();
-           
-
-            Console.WriteLine("enter the city name:");
-            contact.city=Console.ReadLine();
-           
-
-            Console.WriteLine("enter the State:");
-            contact.state = Console.ReadLine();
-          
-
-            Console.WriteLine("enter the zip code:");
-            contact.zip=Console.ReadLine();
-            
-
-            Console.WriteLine("enter the country:");
-            contact.country=Console.ReadLine();
-
-            adressBook.Add(contact);
-
-            foreach (var a in adressBook)
+           /* foreach (var a in adressBook)
             {
                 Console.WriteLine(a.firstName);
-            }
+            }*/
 
             Console.ReadLine();
         }
